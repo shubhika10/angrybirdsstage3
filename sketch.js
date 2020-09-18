6ed851024885e78076e9f860cfcfe7da490cca54
shubhika10 <shubhikarai21@gmail.com>
2020-09-17 20:56:16 -0400

    Add constrained log to the bird

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -33,9 +33,9 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
     bird = new Bird(100,100);
-
+    log6 = new Log(230, 180, 80, PI/2);
+    chain = new Chain(bird.body, log6.body)
 }
 
 function draw(){
@@ -61,4 +61,8 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
+    chain.display();
+
 }
